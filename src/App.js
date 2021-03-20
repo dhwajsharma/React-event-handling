4a31bcf5a973150b73ccd69c1313cdee864dfc51
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-20 22:35:51 +0530

    react event handling using hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showpassword, setShowpassword] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <input
+        type="text"
+        placeholder="Enter username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Enter password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <h2>{username}</h2>
+
+      <h2>{showpassword ? password : "*".repeat(password.length)}</h2>
+      <button onClick={(e) => setShowpassword(!showpassword)} >Show/Hide password</button>
     </div>
   );
 }
